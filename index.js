1341d88b858b2f3e6e8ccd0d22f1d5334193e2a7
Aleksey <alexey@kaerus.eu>
2015-04-26 19:48:05 +0300

    change index.js exports

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = require("./lib/unzippy");
+"use strict";
+
+var Unzippy = require("./lib/unzippy.js"),
+	Parser = require("./lib/parser.js")
+	;
+
+function unzippy(options) {
+	return new Unzippy(options);
+}
+
+unzippy.Unzippy = Unzippy;
+unzippy.Parser = Parser;
+
+module.exports = unzippy;
